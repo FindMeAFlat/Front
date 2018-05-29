454deafe10d941a7de199018ece809bfd0702b04
przemek <host9@o2.pl>
2018-05-29 21:48:12 +0200

    Add autocomplete search.

---
+++ src/components/Search/index.js
@@ -0,0 +1,55 @@
+import React, { Component } from 'react';
+import PlacesAutocomplete, { geocodeByAddress, getLatLng } from 'react-places-autocomplete';
+
+export default class Search extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { address: '' };
+  }
+
+    handleChange = (address) => {
+      this.setState({ address });
+    }
+
+    handleSelect = (address) => {
+      geocodeByAddress(address)
+        .then(results => getLatLng(results[0]));
+    }
+
+    render() {
+      return (
+        <PlacesAutocomplete
+          value={this.state.address}
+          onChange={this.handleChange}
+          onSelect={this.handleSelect}
+        >
+          {({ getInputProps, suggestions, getSuggestionItemProps }) => (
+            <div className="container">
+              <input
+                {...getInputProps({
+                                placeholder: 'Search Places ...',
+                                className: 'location-search-input',
+                            })}
+              />
+              <div className="autocomplete-dropdown-container">
+                {suggestions.map((suggestion) => {
+                                const className = suggestion.active ? 'suggestion-item--active' : 'suggestion-item';
+                                // inline style for demonstration purpose
+                                const style = suggestion.active
+                                    ? { backgroundColor: '#fafafa', cursor: 'pointer' }
+                                    : { backgroundColor: '#ffffff', cursor: 'pointer' };
+                                return (
+                                  <div
+                                    {...getSuggestionItemProps(suggestion, { className, style })}
+                                  >
+                                    <span>{suggestion.description}</span>
+                                  </div>
+                                );
+                            })}
+              </div>
+            </div>
+                )}
+        </PlacesAutocomplete>
+      );
+    }
+}
