707c283ae231b31ed9a6187f4529e9713af4e2fb
przemek <host9@o2.pl>
2018-09-24 21:54:35 +0200

    Get user input coordinates

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,28 +1,18 @@
-export const SELECT_CITY = 'SELECT_CITY';
-export function selectCity(city) {
-  return (dispatch) => {
-    dispatch({
-      type: SELECT_CITY,
-      city,
-    });
-  };
-}
+import * as types from './types';
 
-export const SIGN_IN = 'SIGN_IN';
 export function signIn(userId) {
   return (dispatch) => {
     dispatch({
-      type: SIGN_IN,
+      type: types.SIGN_IN,
       payload: userId,
     });
   };
 }
 
-export const LOG_OUT = 'LOG_OUT';
 export function logOut() {
   return (dispatch) => {
     dispatch({
-      type: LOG_OUT,
+      type: types.LOG_OUT,
       payload: null,
     });
   };
--- src/components/CityChooser/index.js
+++ src/components/CityChooser/index.js
@@ -2,62 +2,66 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import classNames from 'classnames';
-
 import StepNavigator from './../StepNavigator';
+import { selectCity } from './../../actions/cities';
 
-import { selectCity } from './../../actions';
+const CITIES = ['Wrocław', 'Warszawa'];
 
 class CityChooser extends Component {
-    static propTypes = {
-      selectCity: PropTypes.func.isRequired,
-      history: PropTypes.object.isRequired,
+  static propTypes = {
+    selectCity: PropTypes.func.isRequired,
+    history: PropTypes.object.isRequired,
+  };
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      selectedCity: CITIES[0],
     };
+  }
 
-    cities = ['Wrocław', 'Warszawa'];
+  handleSubmitCity = () => {
+    this.props.selectCity(this.state.selectedCity);
+    this.props.history.push('/search');
+  };
 
-    state = {
-      cities: this.cities,
-      selectedCity: this.cities[0],
-    };
+  handleChooseCity = selectedCity => this.setState({selectedCity});
 
-    handleSubmitCity = () => {
-      this.props.selectCity(this.state.selectedCity);
-      this.props.history.push('/search');
-    };
+  createLinkForArm = (city, selected) => `${window.location.origin}/arms/${city.toLowerCase()}_${this.state.selectedCity === city || selected ? 'c' : 'g'}.png`;
 
-    handleChooseCity = selectedCity => this.setState({ selectedCity });
+  render() {
+    return (
+      <div className="city-chooser">
+        <StepNavigator stepNumber={1} stepLabel="Select your city" nextPath="search"
+                       nextHandler={this.handleSubmitCity}/>
 
-    createLinkForArm = (city, selected) => `${window.location.origin}/arms/${city.toLowerCase()}_${this.state.selectedCity === city || selected ? 'c' : 'g'}.png`;
-
-    render() {
-      return (
-        <div className="city-chooser">
-          <StepNavigator stepNumber={1} stepLabel="Select your city" nextPath="search" nextHandler={this.handleSubmitCity}/>
-
-          <div className="cities">
-            {this.state.cities.map(city => (
-              <span
-                className="city"
-                onClick={(e) => this.handleChooseCity(city)}
-                key={city}
-              >
+        <div className="cities">
+          {CITIES.map(city => (
+            <span
+              className="city"
+              onClick={() => this.handleChooseCity(city)}
+              key={city}
+            >
                 <img
                   id={city}
                   className={`city-arm ${this.state.selectedCity === city ? 'selected' : 'not-selected'}`}
                   src={this.createLinkForArm(city, false)}
                   alt={city}
-                  onMouseOver={(e) => {e.target.src=this.createLinkForArm(city, true)}}
-                  onMouseOut={(e) => {e.target.src=this.createLinkForArm(city, false)}}
+                  onMouseOver={(e) => {
+                    e.target.src = this.createLinkForArm(city, true);
+                  }}
+                  onMouseOut={(e) => {
+                    e.target.src = this.createLinkForArm(city, false);
+                  }}
                 />
                 <label className="city-name">{city.toUpperCase()}</label>
               </span>))
-            }
-          </div>
+          }
         </div>
-      );
-    }
+      </div>
+    );
+  }
 }
-
 
 const mapDispatchToProps = dispatch => ({
   selectCity: city => dispatch(selectCity(city)),
