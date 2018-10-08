20f01348bf17fde6794a5c0690d9607bd4327866
franczak <host9@o2.pl>
2018-10-08 10:33:06 +0200

    Use cities from response

--- src/components/CityChooser/index.js
+++ src/components/CityChooser/index.js
@@ -5,8 +5,6 @@ import axios from 'axios';
 import StepNavigator from './../StepNavigator';
 import { selectCity } from './../../actions/cities';
 
-const CITIES = ['Wrocław', 'Warszawa'];
-
 class CityChooser extends Component {
   static propTypes = {
       selectCity: PropTypes.func.isRequired,
@@ -16,14 +14,19 @@ class CityChooser extends Component {
   constructor(props) {
       super(props);
       this.state = {
-          selectedCity: CITIES[0],
+          cities: [],
+          selectedCity: ''
       };
   }
 
   componentDidMount() {
       axios.get(`${process.env.REACT_APP_API_URL}/api/cities`)
           .then((response) => {
-              console.log(response);
+            response = response.data.data;
+              this.setState({
+                cities: response,
+                selectedCity: response[0]
+              })
           })
           .catch((error) => {
               console.error(error);
@@ -37,9 +40,37 @@ class CityChooser extends Component {
 
   handleChooseCity = selectedCity => this.setState({ selectedCity });
 
-  createLinkForArm = (city, selected) => `${window.location.origin}/arms/${city.toLowerCase()}_${this.state.selectedCity === city || selected ? 'c' : 'g'}.png`;
+  createLinkForArm = (city, selected) =>
+  `${window.location.origin}/arms/${city.toLowerCase()}_${this.state.selectedCity === city || selected ? 'c' : 'g'}.png`;
 
   render() {
+    const { cities, selectedCity } = this.state;
+      let arms = null;
+      if (cities.length > 0) {
+        arms = <div className="cities">
+            {cities.map(city => (
+                <span
+                    className="city"
+                    onClick={() => this.handleChooseCity(city)}
+                    key={city}
+                >
+                    <img
+                        id={city}
+                        className={`city-arm ${this.state.selectedCity === city ? 'selected' : 'not-selected'}`}
+                        src={this.createLinkForArm(city, false)}
+                        alt={city}
+                        onMouseOver={(e) => {
+                            e.target.src = this.createLinkForArm(city, true);
+                        }}
+                        onMouseOut={(e) => {
+                            e.target.src = this.createLinkForArm(city, false);
+                        }}
+                    />
+                    <label className="city-name">{city.toUpperCase()}</label>
+                </span>))
+            }
+        </div>
+      }
       return (
           <div className="city-chooser">
               <StepNavigator
@@ -48,30 +79,7 @@ class CityChooser extends Component {
                   nextPath="search"
                   nextHandler={this.handleSubmitCity}
               />
-
-              <div className="cities">
-                  {CITIES.map(city => (
-                      <span
-                          className="city"
-                          onClick={() => this.handleChooseCity(city)}
-                          key={city}
-                      >
-                          <img
-                              id={city}
-                              className={`city-arm ${this.state.selectedCity === city ? 'selected' : 'not-selected'}`}
-                              src={this.createLinkForArm(city, false)}
-                              alt={city}
-                              onMouseOver={(e) => {
-                                  e.target.src = this.createLinkForArm(city, true);
-                              }}
-                              onMouseOut={(e) => {
-                                  e.target.src = this.createLinkForArm(city, false);
-                              }}
-                          />
-                          <label className="city-name">{city.toUpperCase()}</label>
-                      </span>))
-                  }
-              </div>
+            {arms}
           </div>
       );
   }
