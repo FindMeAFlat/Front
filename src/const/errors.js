39ae40dcc2156d4c57eabaecaff77f49b1159617
Karol Bartyzel <karol.bartyzel@eset.pl>
2018-11-16 14:59:17 +0100

    Add throttling for custom api

--- src/const/errors.js
+++ src/const/errors.js
@@ -1,6 +1,7 @@
 export default {
     criteria: {
         url: "Url is not valid or doesn't contain ${lat} and ${lon}",
+        requestsLimit: 'Limit of requests must be a number larger than 50 or empty',
         propertyAccess: "Property access must be strings separated by dot (e.g. 'prop1.prop2.prop3')",
         maxRatingValue: 'Max rating must be a positive number',
         importance: 'Importance must be a number from range 1-100',
